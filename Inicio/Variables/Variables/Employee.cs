1fc7842416b33c146325dfb0bfd98ef86b763d77
Emill <lemito66@gmail.com>
2023-10-17 10:33:43 -0500

    hola mundo y variables

---
+++ Inicio/Variables/Variables/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Employee
+        /// <summary>
+        /// Todas las clases tienen por dentro propiedades, métodos y eventos
+        /// 
+    {
+        public Employee()
+        {
+            Console.WriteLine("Codigo desde el constructor");
+        }
+        public string Name { get; set; }
+
+        public string CompleteName()
+        {
+            return this.Name.ToString() + " Completo";
+        }
+    }
+}
---
+++ Inicio/Variables/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Variables;
+
+Console.WriteLine("Hello, World!");
+
+// Declarando la variable
+int salary = 1000;
+int bonus = 200;
+
+
+// Impresion de las variables
+Console.WriteLine("El salario del trabajador es: "+ salary + bonus);
+// Corregir el error de arriba 
+Console.WriteLine("El salario del trabajador es: "+ (salary + bonus));
+
+Employee empleado = new Employee();
+empleado.Name = "Juan";
+Console.WriteLine(empleado.CompleteName());
+
+
+
+
