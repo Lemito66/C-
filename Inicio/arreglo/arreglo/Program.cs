9f783400cfbacbbfe6e22dc3a9d488f0a73661e4
Emill <lemito66@gmail.com>
2023-10-18 18:40:22 -0500

    Arreglos y web

---
+++ Inicio/arreglo/arreglo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Int32[] value = new Int32[3];
+
+value[0] = 1;
+value[1] = 2;
+value[2] = 3;
+
+// Acceder a elementos válidos
+Console.WriteLine(value[0]);
+Console.WriteLine(value[1]);
+Console.WriteLine(value[2]);
