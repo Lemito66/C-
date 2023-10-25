1d8c61198ae21e20187588d21b2534b7d5ab2b6b
Emill <lemito66@gmail.com>
2023-10-25 08:27:16 -0500

    sobre carga de operadores y metedos

---
+++ Inicio/metodos/metodos/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+void DemoCode()
+{
+    Console.WriteLine("Hola Mundo");
+}
+string GetText()
+{
+    return "Hola Mundo";
+}
+
+DemoCode();
+Console.WriteLine(GetText());
---
+++ Inicio/sobreCargaDeOperadores/sobreCargaDeOperadores/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Tienen el mismo nombre pero diferentes parametros
+
+void StopService(string serviceName, int timeoutMilliseconds)
+{
+    // ...
+}
+
+void StopService(string serviceName, TimeSpan timeout)
+{
+    // ...
+}
+
+// Tienen el mismo nombre y los mismos parametros pero diferentes tipos de retorno
+void StopService(string serviceName, int timeoutMilliseconds)
+{
+    // ...
+}
