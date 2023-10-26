26a783680b8de710116800fab5f95e243f3e1820
Emill <lemito66@gmail.com>
2023-10-26 08:15:14 -0500

    creación de clase Products

---
+++ Inicio/appClienteDemoForms/appClienteDemoForms/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClienteDemoForms
+{
+    internal class Products
+    {
+        public Products (int id, string name, double price, int stocks)
+        {
+            // Este es el constructor, siempre debe tener el mismo nombre de la clase
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.Stocks = stocks;
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stocks { get; set; }
+
+    }
+}
