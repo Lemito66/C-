56b8fe2378201f3068a9c8e8e7c7b27af7160e84
Emill <lemito66@gmail.com>
2023-11-01 10:56:00 -0500

    Serializer

--- Inicio/DemoFiles/DemoFiles/Form1.cs
+++ Inicio/DemoFiles/DemoFiles/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;  // for File, FileInfo, Directory, DirectoryInfo
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace DemoFiles
 {
@@ -20,6 +21,11 @@ namespace DemoFiles
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+            this.saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.saveFileDialog1.CheckPathExists = true;
+            //this.saveFileDialog1.Title = "Save a text file";
+            this.saveFileDialog1.CheckFileExists = true;
             
 
             if (this.saveFileDialog1.ShowDialog() == DialogResult.OK) // OK button pressed
@@ -67,6 +73,42 @@ namespace DemoFiles
             }
 
             
+        }
+
+        MyClass myClass;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Serialize an object
+            myClass = new MyClass(1, "Hello");
+            serialize(myClass);
+            MessageBox.Show("Object serialized");
+
+        }
+
+        public static void serialize(MyClass MC)
+        {
+            Stream stream = File.Open("MyClass.obj", FileMode.Create);
+            BinaryFormatter bwritter = new BinaryFormatter();
+
+            bwritter.Serialize(stream, MC);
+            stream.Close();
+        }
+
+        public static MyClass deserialize(MyClass MC)
+        {
+            Stream streamReader = File.Open("MyClass.obj", FileMode.Open);
+            BinaryFormatter breader = new BinaryFormatter();
+            MyClass mcD = breader.Deserialize(streamReader) as MyClass;
+            streamReader.Close();
+            return mcD;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MyClass _myClass = deserialize(myClass);
+            MessageBox.Show(myClass.MyFirstProperty.ToString() + " " + myClass.MySecondProperty);
+
         }
     }
 }
---
+++ Inicio/DemoFiles/DemoFiles/MyClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFiles
+{
+    [Serializable()] // This class can be serialized
+    public class MyClass : ISerializable
+    {
+        public int MyFirstProperty { get; set; }
+        public string MySecondProperty { get; set; }
+        
+        public MyClass(int a, string b)
+        {
+            this.MyFirstProperty = a;
+            this.MySecondProperty = b;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("MyFirstProperty", this.MyFirstProperty);
+            info.AddValue("MySecondProperty", this.MySecondProperty);
+            
+        }
+
+        public MyClass(SerializationInfo info, StreamingContext context)
+        {
+            this.MyFirstProperty = (int)info.GetValue("MyFirstProperty", typeof(int));
+            this.MySecondProperty = (string)info.GetValue("MySecondProperty", typeof(string));
+            
+        }
+    }
+}
