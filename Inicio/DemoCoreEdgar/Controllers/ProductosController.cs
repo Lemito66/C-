a0699db7b7f34b60dd84fdc9798d981fe244061e
Lemito66 <lemito66@gmail.com>
2023-11-14 11:27:00 -0500

    ejemplo .core web de Edgar

---
+++ Inicio/DemoCoreEdgar/Controllers/ProductosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace webAppDemo33.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Productos()
+        {
+            return "Este es un texto desde el controlador";
+        }
+       public string consultarExitencia(int codigo)
+        {
+
+
+            return "Existencia es de : " + codigo.ToString();
+        }
+    }
+}
