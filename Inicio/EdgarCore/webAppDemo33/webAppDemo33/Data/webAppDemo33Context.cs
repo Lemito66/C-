3d3a02de8e717407a9fa5f04244c70ff6fed1ec5
Emill <lemito66@gmail.com>
2023-11-15 10:41:32 -0500

    Eliminación y descarga de app core

---
+++ Inicio/EdgarCore/webAppDemo33/webAppDemo33/Data/webAppDemo33Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webAppDemo33.Models;
+
+namespace webAppDemo33.Data
+{
+    public class webAppDemo33Context : DbContext
+    {
+        public webAppDemo33Context (DbContextOptions<webAppDemo33Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webAppDemo33.Models.Productos> Productos { get; set; } = default!;
+    }
+}
