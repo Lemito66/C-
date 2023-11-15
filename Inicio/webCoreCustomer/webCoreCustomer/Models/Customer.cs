f6efa3b11d69caa99cbb61761035fc3c39af4257
Emill <lemito66@gmail.com>
2023-11-15 12:57:28 -0500

    notación para poder visualizar placeholder y cambio de nombre del campo en el formulario

--- Inicio/webCoreCustomer/webCoreCustomer/Models/Customer.cs
+++ Inicio/webCoreCustomer/webCoreCustomer/Models/Customer.cs
@@ -5,15 +5,22 @@ namespace webCoreCustomer.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nombre", Prompt ="Ingrese el nombre del cliente", Description ="Nombre del cliente")]
         public string? Name { get; set; }
+        [Display(Name = "Dirección")]
         public string? Address { get; set; }
+        [Display(Name = "Teléfono")]
         [StringLength(10, MinimumLength = 10, ErrorMessage ="Solo es permitido 10 caracteres")]
         public string? Telephone { get; set; }
+        [Display(Name = "Límite de crédito")]
         public decimal CreditLimit { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Fecha de creación")]
         public DateTime CreatedAt { get; set; }
 
         [Range(1, 5, ErrorMessage = "Solo es permitido de 1 a 5")]
+        [Display(Name = "Categoría")]
         public int Category { get; set; }
         
         [DataType(DataType.EmailAddress)]
