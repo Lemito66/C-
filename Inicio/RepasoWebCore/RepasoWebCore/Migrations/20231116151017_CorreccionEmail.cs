4f8737740474741c46cd819b278a1646c8dd559b
Lemito66 <lemito66@gmail.com>
2023-11-16 10:14:52 -0500

    Repaso web core mvc  con clientes

---
+++ Inicio/RepasoWebCore/RepasoWebCore/Migrations/20231116151017_CorreccionEmail.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepasoWebCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorreccionEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Customer",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
