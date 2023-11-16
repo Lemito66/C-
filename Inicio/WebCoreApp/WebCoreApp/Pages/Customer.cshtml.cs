e2b449ab17656ecf8533d303b8fd7705b0511a86
Emill <lemito66@gmail.com>
2023-11-16 11:46:51 -0500

    metodo post en formulario

---
+++ Inicio/WebCoreApp/WebCoreApp/Pages/Customer.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebCoreApp.Pages
+{
+    public class CustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
