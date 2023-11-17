25daf8509945df677c458c6ab0d18664c44b229e
Lemito66 <lemito66@gmail.com>
2023-11-17 10:30:43 -0500

    creación de clase para el api

---
+++ Inicio/StudentsApi/StudentsApi/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [DataType(DataType.Text)]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int age { get; set; }
+    }
+}
