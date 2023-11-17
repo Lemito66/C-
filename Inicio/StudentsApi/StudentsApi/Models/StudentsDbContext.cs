fe43862daa83d34681d5bdde4826eb32b6488279
Lemito66 <lemito66@gmail.com>
2023-11-17 11:00:35 -0500

    Listo Api

---
+++ Inicio/StudentsApi/StudentsApi/Models/StudentsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentsApi.Models
+{
+    public class StudentsDbContext : DbContext
+    {
+        public StudentsDbContext(DbContextOptions<StudentsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null;
+    }
+}
