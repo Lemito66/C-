453fb035faead8782758188341ad9cc3cd00025e
Lemito66 <lemito66@gmail.com>
2023-11-17 11:16:22 -0500

    Lista api con database

---
+++ Inicio/ApiWithDataBase/ApiWithDataBase/Data/ApiWithDataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiWithDataBase.Models;
+
+namespace ApiWithDataBase.Data
+{
+    public class ApiWithDataBaseContext : DbContext
+    {
+        public ApiWithDataBaseContext (DbContextOptions<ApiWithDataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiWithDataBase.Models.Student> Student { get; set; } = default!;
+    }
+}
