3ee8318e97c69302ee7f45c07b42dd3a68158407
Lemito66 <lemito66@gmail.com>
2023-11-24 16:25:39 -0500

    crud de tabla clientes

---
+++ EjercicioHospital/HospitalMetropolitano/HospitalMetropolitano/Data/HospitalMetropolitanoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HospitalMetropolitano.Models;
+
+namespace HospitalMetropolitano.Data
+{
+    public class HospitalMetropolitanoContext : DbContext
+    {
+        public HospitalMetropolitanoContext (DbContextOptions<HospitalMetropolitanoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HospitalMetropolitano.Models.Customer> Customer { get; set; } = default!;
+    }
+}
