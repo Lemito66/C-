328bb1bc0fa727377f98b6bfb2fcf6a6f5321319
Lemito66 <lemito66@gmail.com>
2023-11-24 16:53:00 -0500

    for para servicios listo

--- EjercicioHospital/HospitalMetropolitano/HospitalMetropolitano/Controllers/HomeController.cs
+++ EjercicioHospital/HospitalMetropolitano/HospitalMetropolitano/Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace HospitalMetropolitano.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
