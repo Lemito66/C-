c1558b1dc3fa2fc2a595df1b9f958df88d13df25
Emill <lemito66@gmail.com>
2023-11-30 08:40:02 -0500

    creación de modelo customer

---
+++ APICore/apiCore/apiCore/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using apiCore.Models;
+using Microsoft.EntityFrameworkCore; // Esto se lo instala en los paquetes de NuGet, ahora esta con la  versión 6
+namespace apiCore.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        // Siempre que sea una clase de contexto , debe heredar de DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) // Esto es un constructor que recibe un parametro de tipo DbContextOptions<ApplicationDbContext> y lo pasa al constructor de la clase base DbContext
+        {
+            
+        }
+        public DbSet<Customer> Customers { get; set; } // DbSet es una clase que representa una tabla de la base de datos, en este caso Customers es una tabla de la base de datos
+    }
+}
