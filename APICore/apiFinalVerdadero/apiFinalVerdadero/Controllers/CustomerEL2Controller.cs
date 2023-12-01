041e139dcbce49c3efa8ab99c1e46502553191a6
Emill <lemito66@gmail.com>
2023-12-01 11:15:19 -0500

    nuevas api pero sin complicar

---
+++ APICore/apiFinalVerdadero/apiFinalVerdadero/Controllers/CustomerEL2Controller.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiFinalVerdadero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerEL2Controller : ControllerBase
+    {
+        // GET: api/<CustomerEL2Controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerEL2Controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerEL2Controller>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerEL2Controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerEL2Controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ APICore/apiFinalVerdadero/apiFinalVerdadero/Models/CustomerEL2.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiFinalVerdadero.Models
+{
+    public class CustomerEL2
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Text { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
